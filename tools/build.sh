cec512729c5efd9f98ad81e3626f5e41108cad25
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-02-07 00:59:50 -0500

    prog

--- tools/build.sh
+++ tools/build.sh
@@ -3,15 +3,6 @@
 # https://sipb.mit.edu/doc/safe-shell/
 set -euo pipefail
 
-# INSTALL GTEST FOR TRAVIS
-tree
-cd libraries/googletest
-cmake .
-cmake --build .
-cmake --install .
-cd -
-##########################
-
 mkdir -p build && cd build
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # $(nproc) prints number of processing units available
