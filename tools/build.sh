bd6fe73e3022bf010b1c49d98f7d245d29f48bfd
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-02-07 00:21:33 -0500

    Travis fix attempt

--- tools/build.sh
+++ tools/build.sh
@@ -3,9 +3,21 @@
 # https://sipb.mit.edu/doc/safe-shell/
 set -euo pipefail
 
-mkdir -p build && cd build
+## INSTALL GTEST ON TRAVIS ##
+sudo apt-get install libgtest-dev
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib
+sudo ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
+sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
+cd -
+## END INSTALL GTEST ON TRAVIS ##
 
+
+mkdir -p build && cd build
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # $(nproc) prints number of processing units available
 cmake --build . --config Debug -- -j $(nproc)
 ctest -j $(nproc) --output-on-failure
+./unittests
