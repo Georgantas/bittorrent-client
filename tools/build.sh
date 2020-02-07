38ae1eb767fdc614da9d8498a4e6b3e40b8c8fdb
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-02-06 23:34:47 -0500

    Added Gtest unit tests

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# https://sipb.mit.edu/doc/safe-shell/
 set -euo pipefail
 
 mkdir -p build && cd build
 
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+# $(nproc) prints number of processing units available
 cmake --build . --config Debug -- -j $(nproc)
 ctest -j $(nproc) --output-on-failure
