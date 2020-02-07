1140037cda6d3d0ceeb9d07b8eb6965c2f0d2a43
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-02-07 00:41:57 -0500

    prog

--- tools/build.sh
+++ tools/build.sh
@@ -3,20 +3,21 @@
 # https://sipb.mit.edu/doc/safe-shell/
 set -euo pipefail
 
-## INSTALL GTEST ON TRAVIS ##
-cd /usr/src/gtest
-cmake CMakeLists.txt
-make
-cp *.a /usr/lib
-ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
-ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
+# INSTALL GTEST FOR TRAVIS
+cd libraries/googletest
+cmake .
+cmake --build .
+cmake --install .
 cd -
-## END INSTALL GTEST ON TRAVIS ##
-
+##########################
 
 mkdir -p build && cd build
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # $(nproc) prints number of processing units available
 cmake --build . --config Debug -- -j $(nproc)
+
+# run tests
 ctest -j $(nproc) --output-on-failure
+
+# run unit tests
 ./unittests
