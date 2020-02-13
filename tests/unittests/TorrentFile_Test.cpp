33c66a703af49a77c831e826072ee67b27adc3d5
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-02-12 23:11:00 -0500

    complete test

--- tests/unittests/TorrentFile_Test.cpp
+++ tests/unittests/TorrentFile_Test.cpp
@@ -13,8 +13,10 @@ TEST(TorrentFile_Test, BuildFromStream) {
   EXPECT_EQ(torrentFile.getAnnounce(),
             "http://tracker.archlinux.org:6969/announce");
   EXPECT_EQ(torrentFile.getName(), "archlinux-2019.12.01-x86_64.iso");
-  // EXPECT_EQ(torrentFile.getInfoHash(), );
-  // EXPECT_EQ(torrentFile.getLength(), );
-  // EXPECT_EQ(torrentFile.getPieceLength(), );
-  // EXPECT_EQ(torrentFile.getPiecesHash(), );
+  EXPECT_EQ(torrentFile.getPiecesHash().at(999).at(19), 119);
+  EXPECT_EQ(torrentFile.getInfoHash(),
+            Sha1Hash({222, 232, 106, 127, 166, 242, 134, 169, 215, 76,
+                      54,  32,  20,  97,  106, 15,  245, 228, 132, 61}));
+  EXPECT_EQ(torrentFile.getLength(), 670040064);
+  EXPECT_EQ(torrentFile.getPieceLength(), 524288);
 }
